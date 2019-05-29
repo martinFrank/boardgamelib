fb352dd60d769afa869fc14231fcef4790f93d9a
martin.frank <martin.frank.private@gmail.com>
2019-05-29 06:43:32 +0200

    added third player to test

--- src/test/java/com/github/martinfrank/boardgamelib/GameFlowTest.java
+++ src/test/java/com/github/martinfrank/boardgamelib/GameFlowTest.java
@@ -8,7 +8,7 @@ public class GameFlowTest {
     @Test
     public void testPosition() {
         ExampleGame game = new ExampleGame();
-        ExampleGameSetup setup = new ExampleGameSetup(2);
+        ExampleGameSetup setup = new ExampleGameSetup(3);
         game.setup(setup);
         game.initGame();
 
@@ -16,7 +16,13 @@ public class GameFlowTest {
         Assert.assertTrue(game.isFirstPlayer());
         Assert.assertFalse(game.isLastPlayer());
         game.endPlayersTurn();
+        game.startPlayersTurn();
+        Assert.assertFalse(game.isFirstPlayer());
+        Assert.assertFalse(game.isLastPlayer());
+        game.endPlayersTurn();
+        game.startPlayersTurn();
         Assert.assertFalse(game.isFirstPlayer());
         Assert.assertTrue(game.isLastPlayer());
+        game.endPlayersTurn();
     }
 }
